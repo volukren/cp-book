3cfff2d33a9a5f9deb3020c79fe880a364ad6f06
Nurbek Erkulov <erkulovmail@gmail.com>
2022-06-20 00:36:15 +0600

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+#define int long long
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((x).size())
+
+using namespace std;
+
+int32_t main() {
+    cin.tie(nullptr); 
+    ios::sync_with_stdio(false);
+    
+    return 0;
+}
